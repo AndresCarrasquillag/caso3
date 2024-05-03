e0b76963dff6b82019b6573a7d3074b3e0fd941e
AndresCarrasquillag <j.carrasquillag@uniandes.edu.co>
2024-05-02 23:45:08 -0500

    del 1 al 7

--- caso3/src/Cliente.java
+++ caso3/src/Cliente.java
@@ -1,19 +1,38 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
 import java.net.Socket;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 
 public class Cliente {
-    private Socket socket;
     private PublicKey publicKey;
-    private long p;
-    private long g;
-    private long x;
+    public static final int PUERTO = 3400;
+    public static final String SERVIDOR = "localhost";
 
+    public static void main(String args[]) throws IOException {
+        Socket socket = null;
+        try {
+            socket = new Socket(SERVIDOR, PUERTO);
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            // Punto 1
+            SecureRandom random = new SecureRandom();
+            System.out.println(".");
+            BigInteger reto = new BigInteger(1024, random);
+            out.writeObject(reto);
+            // PUNTO 4
+            byte[] r = (byte[] )inputStream.readObject();
+            // descrifrar y si es igual a reto entonces envía ok
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 
 
     public Cliente(PublicKey publicKey, long p, long g, long x) {
         this.publicKey = publicKey;
-        this.p = p;
-        this.g = g;
-        this.x = x;
     }
 }
--- caso3/src/Servidor.java
+++ caso3/src/Servidor.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.math.BigInteger;
 import java.net.ServerSocket;
@@ -10,10 +11,7 @@ import javax.crypto.spec.DHParameterSpec;
 
 public class Servidor {
     public static final int PUERTO = 3400;
-    private PrivateKey privateKey;
-    private long p;
-    private long g;
-    private long x;
+    private static PrivateKey privateKey;
     
     public static void main(String args[]) throws IOException {
         ServerSocket ss = null;
@@ -28,18 +26,29 @@ public class Servidor {
 
         while (continuar) {
             Socket socket = ss.accept();
+            System.out.println("Servidor escuchando en el puerto " + PUERTO);
             try {
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
 
+                // PUNTO 2
+                BigInteger reto = (BigInteger) inputStream.readObject();
+                System.out.println(reto);
+                // PUNTO 3
+                out.writeObject(CifradoAsimetrico.cifrar(privateKey, "AES/CBC/PKCS5Padding", reto.toString()));
                 /// PUNTO 6
                 BigInteger[] params = DiffieHallman.generarParams();
                 BigInteger g = params[0];
                 BigInteger p = params[1];
                 BigInteger gxmodp = params[2];
-                
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                // PUNTO 7
                 out.writeObject(g);
                 out.writeObject(p);
                 out.writeObject(gxmodp);
+                out.writeObject(CifradoAsimetrico.cifrar(privateKey, "AES/CBC/PKCS5Padding", g.toString()));
+                out.writeObject(CifradoAsimetrico.cifrar(privateKey, "AES/CBC/PKCS5Padding", p.toString()));
+                out.writeObject(CifradoAsimetrico.cifrar(privateKey, "AES/CBC/PKCS5Padding", gxmodp.toString()));
+
                 
             } catch (Exception e) {
                 // TODO: handle exception
@@ -49,8 +58,5 @@ public class Servidor {
     
     public Servidor(PrivateKey privateKey, long p, long g, long x) {
         this.privateKey = privateKey;
-        this.p = p;
-        this.g = g;
-        this.x = x;
     }
 };
